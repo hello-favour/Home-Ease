47d46ed5f941dbe7cf2020cda9f7903153a14416
Favour <hello.williamsfav@gmail.com>
2024-11-19 07:53:59 +0100

    added appbar with tabs on the home screen

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:home_ease/core/constants/app_colors.dart';
+import 'package:home_ease/utils/app_textfield.dart';
+import 'package:home_ease/utils/extension.dart';
+import 'package:sizer/sizer.dart';
 
 class HomeView extends ConsumerStatefulWidget {
   const HomeView({super.key});
@@ -11,6 +15,71 @@ class HomeView extends ConsumerStatefulWidget {
 class _HomeViewState extends ConsumerState<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 6.w),
+          child: Column(
+            children: [
+              2.sH,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Hey Dennis👋🏾",
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      Icon(
+                        Icons.notifications,
+                        color: AppColors.blackColor,
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "Find your perfect \nfuniture",
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ],
+              ),
+              2.sH,
+              const AppTextfield(
+                label: "Search",
+                textInputType: TextInputType.text,
+              ),
+              2.sH,
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(
+                    5,
+                    (_) => Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          color: AppColors.blackColor,
+                          child: Text(
+                            "All Products",
+                            style:
+                                Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                      color: AppColors.whiteColor,
+                                    ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
