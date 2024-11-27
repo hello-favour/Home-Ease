8dabdcddad9430b011de1f8d40a6794959fd7692
Favour <hello.williamsfav@gmail.com>
2024-11-27 19:16:43 +0100

    Added Firebase SDK to the project and initialized Firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:home_ease/core/constants/app_router.dart';
+import 'package:home_ease/firebase_options.dart';
 import 'package:home_ease/my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   CustomNavigationHelper.instance;
   runApp(
     const ProviderScope(
